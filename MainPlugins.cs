ac6946de08aa0c785057f0e3557f470cafd0147a
bogdhb2000-commits <bogdhb2000@gmail.com>
2026-09-06 17:18:20 +0500

    Update MainPlugins.cs

--- MainPlugins.cs
+++ MainPlugins.cs
@@ -1,6 +1,7 @@
 using System;
 using BepInEx;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace MySilentPullMod
 {
@@ -8,12 +9,12 @@ namespace MySilentPullMod
     public class SilentPullMod : BaseUnityPlugin
     {
         // Настройки Playspace Abuse
-        public static float playspaceAbusePower = 0.08f;
+        public static float playspaceAbusePower = 0.05f;
 
         void Update()
         {
-            // Активация при нажатии на ПРАВЫЙ джойстик (Right Controller Click)
-            if (ControllerInputPoller.instance != null && ControllerInputPoller.instance.rightControllerPrimary2DAxisClick)
+            // Активация при нажатии на ЛЕВЫЙ джойстик (клик по левому стику)
+            if (IsJoystickClicked(XRNode.LeftHand))
             {
                 PlayspaceAbuse();
             }
@@ -22,6 +23,18 @@ namespace MySilentPullMod
         private void PlayspaceAbuse()
         {
             GorillaTagger.Instance.transform.position += GorillaTagger.Instance.headCollider.transform.forward * playspaceAbusePower;
+        }
+
+        // Проверка клика по джойстику через XR Input
+        private bool IsJoystickClicked(XRNode node)
+        {
+            InputDevice device = InputDevices.GetDeviceAtXRNode(node);
+            if (device.isValid && device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool isPressed))
+            {
+                return isPressed;
+            }
+
+            return false;
         }
     }
 }
