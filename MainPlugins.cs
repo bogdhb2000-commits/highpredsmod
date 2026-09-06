4aef84fcd053698d1891b31ac255e4b1bd6d6cc0
bogdhb2000-commits <bogdhb2000@gmail.com>
2026-09-06 19:12:54 +0500

    Update MainPlugins.cs

--- MainPlugins.cs
+++ MainPlugins.cs
@@ -5,27 +5,9 @@ using UnityEngine;
 namespace MyVelocityPredictionMod
 {
     [BepInPlugin("com.username.velocityprediction", "Velocity Prediction Mod", "1.0.0")]
-    public class VelocityPredictionMod : BaseUnityPlugin
+    public class MainPlugins : BaseUnityPlugin
     {
-        // Сила предикшена
         public static float predCount = 0.4f;
-
-        private GameObject lvT;
-        private GameObject rvT;
-
-        void Start()
-        {
-            // Создаем трекеры скорости
-            lvT = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Destroy(lvT.GetComponent<BoxCollider>());
-            lvT.GetComponent<Renderer>().enabled = false;
-            lvT.AddComponent<GorillaVelocityTracker>();
-
-            rvT = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Destroy(rvT.GetComponent<BoxCollider>());
-            rvT.GetComponent<Renderer>().enabled = false;
-            rvT.AddComponent<GorillaVelocityTracker>();
-        }
 
         void Update()
         {
@@ -40,19 +22,13 @@ namespace MyVelocityPredictionMod
         {
             if (GorillaTagger.Instance == null) return;
 
-            // Расчет позиции трекеров
-            lvT.transform.position = GorillaTagger.Instance.headCollider.transform.position - GorillaTagger.Instance.leftHandTransform.position;
-            rvT.transform.position = GorillaTagger.Instance.headCollider.transform.position - GorillaTagger.Instance.rightHandTransform.position;
+            // Получаем скорость рук напрямую через Rigidbody игроков
+            Vector3 leftVelocity = GorillaTagger.Instance.leftHandTransform.GetComponent<Rigidbody>()?.velocity ?? Vector3.zero;
+            Vector3 rightVelocity = GorillaTagger.Instance.rightHandTransform.GetComponent<Rigidbody>()?.velocity ?? Vector3.zero;
 
-            // Смещение рук
-            GorillaTagger.Instance.offlineVRRig.leftHand.rigTarget.transform.position -= lvT.GetComponent<GorillaVelocityTracker>().GetAverageVelocity(true, 0) * predCount;
-            GorillaTagger.Instance.offlineVRRig.rightHand.rigTarget.transform.position -= rvT.GetComponent<GorillaVelocityTracker>().GetAverageVelocity(true, 0) * predCount;
-        }
-
-        private void OnDestroy()
-        {
-            if (lvT != null) Destroy(lvT);
-            if (rvT != null) Destroy(rvT);
+            // Применяем смещение
+            GorillaTagger.Instance.offlineVRRig.leftHand.rigTarget.transform.position -= leftVelocity * predCount;
+            GorillaTagger.Instance.offlineVRRig.rightHand.rigTarget.transform.position -= rightVelocity * predCount;
         }
     }
 }
