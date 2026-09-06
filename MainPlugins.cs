206302cb15e4423ad792e061a2b1e83a67e48db4
bogdhb2000-commits <bogdhb2000@gmail.com>
2026-09-06 17:35:17 +0500

    Update MainPlugins.cs

--- MainPlugins.cs
+++ MainPlugins.cs
@@ -8,13 +8,16 @@ namespace MySilentPullMod
     [BepInPlugin("com.username.silentpullmod", "Silent Pull Mod", "1.0.0")]
     public class SilentPullMod : BaseUnityPlugin
     {
-        // Настройки Playspace Abuse
+        // Сила смещения
         public static float playspaceAbusePower = 0.05f;
 
         void Update()
         {
-            // Активация при нажатии на ЛЕВЫЙ джойстик (клик по левому стику)
-            if (IsJoystickClicked(XRNode.LeftHand))
+            // Проверяем наклон левого джойстика
+            Vector2 joystick = GetJoystickInput(XRNode.LeftHand);
+
+            // Если левый стик наклонен вперед (значение по Y больше 0.5)
+            if (joystick.y > 0.5f)
             {
                 PlayspaceAbuse();
             }
@@ -25,16 +28,15 @@ namespace MySilentPullMod
             GorillaTagger.Instance.transform.position += GorillaTagger.Instance.headCollider.transform.forward * playspaceAbusePower;
         }
 
-        // Проверка клика по джойстику через XR Input
-        private bool IsJoystickClicked(XRNode node)
+        // Получаем точный вектор наклона джойстика через XR Input
+        private Vector2 GetJoystickInput(XRNode node)
         {
             InputDevice device = InputDevices.GetDeviceAtXRNode(node);
-            if (device.isValid && device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool isPressed))
+            if (device.isValid && device.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 axis))
             {
-                return isPressed;
+                return axis;
             }
-
-            return false;
+            return Vector2.zero;
         }
     }
 }
