349c2bfa512fee4a26ab2bf3ef2daa2d4a40ed64
bogdhb2000-commits <bogdhb2000@gmail.com>
2026-09-10 22:28:05 +0500

    Update MainPlugins.cs

--- MainPlugins.cs
+++ MainPlugins.cs
@@ -8,7 +8,7 @@ namespace MySilentPullMod
     public class SilentPullMod : BaseUnityPlugin
     {
         // Сила смещения
-        public static float playspaceAbusePower = 0.09f;
+        public static float playspaceAbusePower = 0.3f;
 
         void Update()
         {
